49189ba98bf1860b5e6f209fe9109e9dfb9e968d
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-01-28 23:13:42 +0600

    #3 Внесены поправки по формату времени

--- src/component/Message/Message.js
+++ src/component/Message/Message.js
@@ -3,13 +3,17 @@ import { React } from 'react';
 import { Toast, ToastBody, ToastHeader } from 'reactstrap';
 
 const Message = props => {
+    let changeFormat = datetime => {
+        let date = new Date(datetime).toLocaleString();
+        return date
+    }
     let printMessages = props.array.map(data => {
         return (
             <div className="my-2 rounded" key={data._id}>
             <Toast>
             <ToastHeader>
             <h5 className='title'>{data.author}</h5>
-            <h5 className='title'>{data.datetime}</h5> 
+            <h5 className='title_date'>{changeFormat(data.datetime)}</h5> 
             </ToastHeader>
             <ToastBody className='text_wrapper'> 
             {data.message}
