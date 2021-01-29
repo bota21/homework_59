f471eb075a802decf328d346bdf8b1bbf0fafba2
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-01-29 16:53:01 +0600

    fcsijg

--- src/component/Message/Message.js
+++ src/component/Message/Message.js
@@ -1,30 +1,10 @@
 import './Message.css';
-import { React } from 'react';
-import { Toast, ToastBody, ToastHeader } from 'reactstrap';
-
 const Message = props => {
-    let changeFormat = datetime => {
-        let date = new Date(datetime).toLocaleString();
-        return date
-    }
-    let printMessages = props.array.map(data => {
         return (
-            <div className="my-2 rounded" key={data._id}>
-            <Toast>
-            <ToastHeader>
-            <h5 className='title'>{data.author}</h5>
-            <h5 className='title_date'>{changeFormat(data.datetime)}</h5> 
-            </ToastHeader>
-            <ToastBody className='text_wrapper'> 
-            {data.message}
-            </ToastBody>
-            </Toast>
+            <div style={{display: props.show ? 'block' : 'none'}}>
+                First input           
             </div> 
             )
-        })
-        return <div>
-        {printMessages}
-        </div>
     };
     
     export default Message
--- src/container/App.js
+++ src/container/App.js
@@ -1,42 +1,29 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Message from '../component/Message/Message';
-import ChatForm from '../component/ChatForm/ChatForm';
+import './App.css';
 
-function App() {
-  let url = 'http://146.185.154.90:8000/messages';
-  const [messages, setMessages] = useState([]);
-  
-  useEffect(async() => {
-    try{
-      let getUrl = axios.get(url);
-      await getUrl.then(res => {
-        setMessages(res.data)
-      })
-    } catch(error) {
-      console.error(error);
-    }   
-  }, [messages])
 
-  let submitMessage = (async(e, data) => {  
-    e.preventDefault();
-    const newValue = new URLSearchParams();
-    newValue.set('author', data.author);
-    newValue.set('message', data.message);
-    await axios({
-      method: 'post',
-      url: url,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      data: newValue
-    })
-    .then(res => console.log(res))
-    .catch(error => console.log(error))
-  });
+const App = () => {
+  const [show, setShow] = useState(false);
+  let changeInput = (num) => {
+    num === 1 ? setShow(false) : setShow(true)
+  } 
   return (
-    <div className="App">      
-    <ChatForm submit={submitMessage}  />
-    <Message array={messages} /> 
+    <div className="App">    
+    <label>
+    <input type="radio" name='input' onChange={() => changeInput(0)}/>
+    input1
+      </label>  
+      <label>
+    <input type="radio" name='input' onChange={() => changeInput(1)}/>
+    input2
+    </label>
+
+
+<Message show={show}/>
+
+    {/* <div style={{display: show ? 'block' : 'none'}}>First input</div> */}
+    <div style={{display: !show ? 'block' : 'none'}}>Secont input</div>
     </div>
     );
   }
