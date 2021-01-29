fe451e7afa774ea92e3465230311444ec908b401
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-01-29 17:11:51 +0600

    #1 Проект снова запушен, так как предыдущий push был внесен ошибочно

--- src/component/Message/Message.js
+++ src/component/Message/Message.js
@@ -1,10 +1,30 @@
 import './Message.css';
+import { React } from 'react';
+import { Toast, ToastBody, ToastHeader } from 'reactstrap';
+
 const Message = props => {
+    let changeFormat = datetime => {
+        let date = new Date(datetime).toLocaleString();
+        return date
+    }
+    let printMessages = props.array.map(data => {
         return (
-            <div style={{display: props.show ? 'block' : 'none'}}>
-                First input           
+            <div className="my-2 rounded" key={data._id}>
+            <Toast>
+            <ToastHeader>
+            <h5 className='title'>{data.author}</h5>
+            <h5 className='title_date'>{changeFormat(data.datetime)}</h5> 
+            </ToastHeader>
+            <ToastBody className='text_wrapper'> 
+            {data.message}
+            </ToastBody>
+            </Toast>
             </div> 
             )
+        })
+        return <div>
+        {printMessages}
+        </div>
     };
     
     export default Message
